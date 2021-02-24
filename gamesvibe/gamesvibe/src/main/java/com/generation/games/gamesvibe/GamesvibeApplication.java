83f4036be1f266c301b5638124c374abd7d54d8e
RuiCoders <ruyx@live.com>
2021-02-24 10:41:00 -0300

    Adicionando novo projeto games vibes

---
+++ gamesvibe/gamesvibe/src/main/java/com/generation/games/gamesvibe/GamesvibeApplication.java
@@ -0,0 +1,13 @@
+package com.generation.games.gamesvibe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesvibeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesvibeApplication.class, args);
+	}
+
+}
---
+++ gamesvibe/gamesvibe/src/test/java/com/generation/games/gamesvibe/GamesvibeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.games.gamesvibe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesvibeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
